24b77592a6df9bcb0b16a5838791e81716f94f72
Jeremy Herzog <herzo175@umn.edu>
2018-01-28 11:07:33 -0600

    add way for servers to communicate

--- python_example.py
+++ python_example.py
@@ -5,6 +5,8 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 import pandas
 
+STATE = ''
+
 
 def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4=0):
     print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
@@ -28,6 +30,10 @@ def main():
     server_thread = threading.Thread(target=server.serve_forever)
     print("Serving on {}".format(server.server_address))
     server_thread.start()
+
     time.sleep(5)
+    global STATE
+    STATE = 'done'
+
     server.shutdown()
     print("Done serving on {}".format(server.server_address))
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import os
 import time
 import threading
-import python_example
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from random import randint
+
+from python_example import main
 
 
 app = Flask(__name__)
@@ -14,9 +15,12 @@ CORS(app)
 
 @app.route('/send_data', methods=['POST'])
 def login():
-    python_example.main()
-    return jsonify({'pct_chance': str(randint(0, 100))})
+    main()
 
+    while True:
+        from python_example import STATE
+        if STATE == 'done':
+            return jsonify({'pct_chance': str(randint(0, 100))})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT')))
